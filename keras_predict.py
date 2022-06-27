b33b4335c163f6bda659965233c5290237692cfc
Marvin Haschker <marvin@haschker.me>
2022-06-27 15:17:23 +0200

    Add confusion matrix

--- keras_predict.py
+++ keras_predict.py
@@ -1,4 +1,7 @@
 import argparse
+
+from sklearn.metrics import ConfusionMatrixDisplay
+
 from move_dataset import MoveDataset
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
@@ -9,6 +12,7 @@ from sklearn import metrics
 import numpy as np
 from random import uniform
 import pandas as pd
+import matplotlib.pyplot as plt
 
 dataset = MoveDataset(pickle_file='move_data.pkl')
 
@@ -17,6 +21,10 @@ X = preprocessing.StandardScaler().fit_transform(X.reshape(-1, 256)).reshape(-1,
 
 # One hot vector to single value vector
 y = np.argmax(y_onehot, axis=1)
+
+# Do a split of training and test data with a 80%/20% split
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2)
+
 
 # Source: https://github.com/margitantal68/sapimouse/blob/40b5ea6cf10c6f1d64b9dd0427d21138cc4f75e2/util/oneclass.py#L40
 def compute_AUC_EER(positive_scores, negative_scores):
@@ -31,6 +39,7 @@ def compute_AUC_EER(positive_scores, negative_scores):
     EER_fnr = fnr[np.argmin(np.absolute((fnr - fpr)))]
     EER = 0.5 * (EER_fpr + EER_fnr)
     return roc_auc, EER
+
 
 # Source: https://github.com/margitantal68/sapimouse/blob/40b5ea6cf10c6f1d64b9dd0427d21138cc4f75e2/util/oneclass.py#L138
 def score_normalization(positive_scores, negative_scores):
@@ -55,6 +64,12 @@ def score_normalization(positive_scores, negative_scores):
     return positive_scores, negative_scores
 
 
+all_accuracies = []
+total_confusion_matrix = np.zeros((2, 2))
+
+total_y_test = np.empty(0)
+total_y_pred = np.empty(0)
+
 if __name__ == "__main__":
     # Extract parameters from command line arguments
     parser = argparse.ArgumentParser()
@@ -64,10 +79,11 @@ if __name__ == "__main__":
     model = truncate_model(tf.keras.models.load_model(args.model))
 
     for userid in range(dataset.unique_user_count()):
-        if np.count_nonzero(y == userid) == 0:
+        if np.count_nonzero(Y_train == userid) == 0:
             continue
-        X_positive = X[y == userid]
-        X_negative = X[y != userid]
+        print('User: {}/{}'.format(userid, dataset.unique_user_count()))
+        X_positive = X_train[Y_train == userid]
+        X_negative = X_train[Y_train != userid]
 
         # Extract new features from dx, dy vectors
         X_positive_features = get_model_output_features(model, X_positive)
@@ -76,10 +92,37 @@ if __name__ == "__main__":
         clf = OneClassSVM(gamma='scale')
         clf.fit(X_positive_features)
 
-        positive_scores = clf.score_samples(X_positive_features)
-        negative_scores = clf.score_samples(X_negative_features)
+        X_positive_test = X_test[Y_test == userid]
+        X_negative_test = X_test[Y_test != userid]
 
-        auc, eer = compute_AUC_EER(positive_scores, negative_scores)
+        if X_positive_test.shape[0] > 0 and X_negative_test.shape[0] > 0:
+            X_positive_test = get_model_output_features(model, X_positive_test)
+            X_negative_test = get_model_output_features(model, X_negative_test)
 
-        positive_scores, negative_scores = score_normalization(positive_scores, negative_scores)
-        print('auc:', auc, 'eer:', eer)
+            y_pred_positive = clf.predict(X_positive_test)
+            y_pred_negative = clf.predict(X_negative_test)
+
+            y_test = np.empty(len(y_pred_positive) + len(y_pred_negative))
+            y_test[:len(y_pred_positive)] = 1
+            y_test[len(y_pred_positive):] = -1
+            y_pred_total = np.append(y_pred_positive, y_pred_negative)
+            all_accuracies.append(metrics.accuracy_score(y_test, y_pred_total))
+            print(all_accuracies[-1])
+
+            total_y_test = np.append(total_y_test, y_test)
+            total_y_pred = np.append(total_y_pred, y_pred_total)
+
+        # positive_scores = clf.score_samples(X_positive)
+        # negative_scores = clf.score_samples(X_negative)
+
+        # auc, eer = compute_AUC_EER(positive_scores, negative_scores)
+
+        # positive_scores, negative_scores = score_normalization(positive_scores, negative_scores)
+        # print('auc:', auc, 'eer:', eer)
+
+# print mean accuracy
+print('mean accuracy:', np.mean(all_accuracies))
+
+ConfusionMatrixDisplay.from_predictions(total_y_test, total_y_pred,
+                                        display_labels=['Actual User', 'Impostor']).plot()
+plt.show()
--- random_forest_new.py
+++ random_forest_new.py
@@ -1,4 +1,5 @@
-from sklearn import datasets, preprocessing
+from sklearn import preprocessing
+from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
@@ -7,6 +8,7 @@ from move_dataset import MoveDataset
 from keras_utils import truncate_model, get_model_output_features
 import tensorflow.keras as keras
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 dataset = MoveDataset(pickle_file='move_data.pkl')
 
@@ -17,12 +19,16 @@ X = preprocessing.StandardScaler().fit_transform(X.reshape(-1, 256)).reshape(-1,
 model = truncate_model(tf.keras.models.load_model('final_model.h5'))
 
 # Do a split of training and test data with a 80%/20% split
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
 X_train_new = get_model_output_features(model, X_train)
 X_test_new = get_model_output_features(model, X_test)
 
 all_accuracies = []
+total_confusion_matrix = np.zeros((2, 2))
+
+total_y_test = np.empty(0)
+total_y_pred = np.empty(0)
 
 for userid in range(dataset.unique_user_count()):
     # Create a y vector for which the value is 1 if it is equal to the userid or 0 otherwise.
@@ -36,8 +42,13 @@ for userid in range(dataset.unique_user_count()):
     clf.fit(X_train_new, y_train)
     y_pred = clf.predict(X_test_new)
     all_accuracies.append(metrics.accuracy_score(y_test, y_pred))
-    print("Accuracy:", all_accuracies[-1])
+
+    # Append y_test and y_pred to the total_y_test and total_y_pred array
+    total_y_test = np.append(total_y_test, y_test)
+    total_y_pred = np.append(total_y_pred, y_pred)
 
 print("Mean accuracy:", np.mean(all_accuracies))
 
-
+ConfusionMatrixDisplay.from_predictions(total_y_test, total_y_pred,
+                                        display_labels=['Actual User', 'Impostor']).plot()
+plt.show()
